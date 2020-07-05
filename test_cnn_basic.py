fc7a1ecc4e1e9fdbf0bcb2ab3a3d272f2b891bb6
Shawn <shawntaylor@weecology.org>
2020-07-05 14:24:49 -0600

    load images into memory for the fully trained model
    - the ImageDataGenerator will read every image every single iteration,
    this will load all images into numpy arrays, which should speed
    things up.

--- test_cnn_basic.py
+++ test_cnn_basic.py
@@ -3,11 +3,12 @@ from glob import glob
 import pandas as pd
 import numpy as np
 
-#import matplotlib.pyplot as plt
-
 from tensorflow import keras
 from tensorflow.keras.layers import Dense, Input, Dropout, Flatten
-from keras_preprocessing.image import ImageDataGenerator
+from keras_preprocessing.image import (ImageDataGenerator,
+                                       load_img,
+                                       img_to_array,
+                                       )
 from tensorflow.keras.utils import to_categorical
 
 from sklearn.utils.class_weight import compute_sample_weight
@@ -15,10 +16,13 @@ from sklearn.utils.class_weight import compute_sample_weight
 from vgg16_places_365 import VGG16_Places365
 
 
-image_info = pd.read_csv('first_test_cnn/crop_only.csv')
+image_dir = 'data/phenocam_images'
+train_sample_size = 5000
 
-# testing only with 2 of the more common/distinct classes. growth state (1) and harvested (5)
-#image_info = image_info[image_info.field_status_crop.isin([0,1,5])]
+target_size = (224,224)
+batch_size  = 64
+
+image_info = pd.read_csv('first_test_cnn/crop_only.csv')
 
 print('sample sizes: {s}'.format(s=image_info.field_status_crop.value_counts()))
 
@@ -38,20 +42,38 @@ for name_i, n in enumerate(class_names):
 validation_images = image_info.sample(frac=0.2)
 train_images      = image_info[~image_info.index.isin(validation_images.index)]
 train_images['sample_weight'] = compute_sample_weight('balanced', train_images.field_status_crop)
-train_images = train_images.sample(n=5000, replace=True, weights='sample_weight')
+train_images = train_images.sample(n=train_sample_size, replace=True, weights='sample_weight')
 
-# n_classes = len(image_info.field_status_crop.unique())
-# class_counts = image_info.groupby('field_status_crop').size().reset_index(name='counts')
-# weights = {0:0 , 1:0.34, 2:0.98219, 3:0, 4:0.975}
+
+# Load in images to numpy arrays
+def load_imgs_from_df(df, x_col, img_dir, target_size, data_format='channels_last'):
+    n_images = len(df)
+    img_array = np.zeros((n_images,) + target_size + (3,))
+    
+    for i,j in enumerate(df[x_col]):
+        img = load_img(img_dir + j,
+                       color_mode='rgb',
+                       target_size=target_size)
+        img_array[i] = img_to_array(img, data_format=data_format)
+        # Pillow images should be closed after `load_img`,
+        # but not PIL images.
+        if hasattr(img, 'close'):
+            img.close()
+    
+    return img_array
+    
+
+train_x = load_imgs_from_df(train_images, x_col='file', target_size=target_size, data_format='channels_last')
+train_y = train_images[class_names].values
+
+val_x = load_imgs_from_df(validation_images, x_col='file', target_size=target_size, data_format='channels_last')
+val_y = train_images[class_names].values
 
 def scale_images(x):
     x /= 127.5
     x -= 1
     return x 
 
-image_dir = 'data/phenocam_images'
-target_size = (224,224)
-batch_size  = 64
 train_generator = ImageDataGenerator(preprocessing_function=scale_images,
                                      vertical_flip = True,
                                      horizontal_flip = True,
@@ -61,34 +83,26 @@ train_generator = ImageDataGenerator(preprocessing_function=scale_images,
                                      height_shift_range = [-0.25,0,0.25],
                                      shear_range = 45,
                                      #brightness_range = [0.2,1],
-                                     fill_mode='reflect').flow_from_dataframe(
-                                         train_images, 
-                                         directory = image_dir,
-                                         target_size = target_size,
+                                     fill_mode='reflect').flow(
+                                         x = train_x,
+                                         y = train_y,
+                                         shuffle = True,
                                          batch_size = batch_size,
-                                         validation_split=0,
-                                         x_col = 'file',
-                                         y_col = class_names,
-                                         class_mode = 'raw'
                                          )
 
 # No random transformations for test images                                        
-validation_generator  = ImageDataGenerator(preprocessing_function=scale_images).flow_from_dataframe(
-                                         validation_images, 
-                                         directory = image_dir,
-                                         target_size = target_size,
+validation_generator  = ImageDataGenerator(preprocessing_function=scale_images).flow(
+                                         x = val_x,
+                                         y = val_y,
+                                         shuffle = True,
                                          batch_size = batch_size,
-                                         shuffle = False,
-                                         x_col = 'file',
-                                         y_col = class_names,
-                                         class_mode = 'raw'
                                          )
 
 # Example from https://riptutorial.com/keras/example/32608/transfer-learning-using-keras-and-vgg
 #base_model = VGG16_Places365(
 base_model = keras.applications.VGG16(
     weights=None,  # Load weights pre-trained on ImageNet.
-    input_shape= (224,224,3),
+    input_shape= target_size + (3,),
     include_top=True,
     classes=len(class_names)
 )  # Do not include the ImageNet classifier at the top.
